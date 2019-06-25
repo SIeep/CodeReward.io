be8451c1da5aba65def102e876f52b2efd03f203
SIeep <brandon_n_evans@live.com>
2019-06-25 13:07:01 -0500

    add Welcome.vue and simple body text

---
+++ frontend/src/views/Welcome.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>This is the login page</h1>
+    <div class="login">
+      <form v-on:submit.prevent="submit()">
+        <div class="row">
+          <label for="my-username" class="sr-only">Username</label>
+          <br>
+          <input id="my-email" type="text" name="Username" title="Enter Username" placeholder="Your Username" v-model="username"/>
+        </div>
+        <div class="row">
+          <label for="my-password" class="sr-only">Password</label>
+          <br>
+          <input id="my-password" type="text" name="password" title="Enter password" placeholder="Your password" v-model="password"/>
+        </div>
+      </form>
+      <button>Login</button>
+    </div>
+  </div>
+</template>
