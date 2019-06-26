883c5787514f7b38e3ee0c424b33b452e5651a5a
SIeep <brandon_n_evans@live.com>
2019-06-26 10:45:23 -0500

    add coding platform json parsing logic WIP

--- frontend/src/views/Welcome.vue
+++ frontend/src/views/Welcome.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is the login page</h1>
+    <!-- <h1>This is the login page</h1> -->
     <div class="list-accounts">
-      <h2>This is test GET request to fetch and list all users from DB:</h2>
+      <h2>This is test GET request to fetch and list all users from my DB:</h2>
       <div v-for="user in users">
         Username: {{user}}
+      </div>
+      {{teamTreehouseAccount}}
+      <h2>This is test GET request to teamtreehouse for my profile:</h2>
+      <div v-for="badge in teamTreehouseAccount.badges">
+        Team Treehouse Account badge: {{badge.name}}
+      </div>
+      <h2>This is test GET request to codecademy for my profile:</h2>
+      <div>
+        {{codeCademyAccount}}
       </div>
     </div>
     <br>
@@ -28,17 +37,29 @@
 
 <script type="text/javascript">
   import axios from "axios";
+  import Mercury from "@postlight/mercury-parser";
+
+  const url="https://www.codecademy.com/lnsomniac";
+  Mercury.parse(url).then(result => this.codeCademyAccount = response.data);
 
   export default {
     data: function() {
       return {
         users: "",
+        teamTreehouseAccount: "",
+        // codeCademyAccount: "",
       };
     },
     created: function() {
       axios.get("/api/users").then(response => {
         this.users = response.data;
       });
-    }
+      axios.get("https://teamtreehouse.com/brandonevans5.json").then(response => {
+        this.teamTreehouseAccount = response.data;
+      });
+      // axios.get("https://www.codecademy.com/users/lnsomniac").then(response => {
+      //   this.codeCademyAccount = response.data;
+      // });
+    },
   }
 </script>
